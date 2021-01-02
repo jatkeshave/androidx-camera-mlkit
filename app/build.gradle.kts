135b9015ec4d35cbb97e83538e70d43d4d697b56
Keshav Jat <jatkeshave@gmail.com>
2021-01-02 16:51:42 +0530

    fix: android material components dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,5 +43,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 
-    implementation("implementation 'com.google.android.material:material:1.2.1")
+    implementation("com.google.android.material:material:1.2.1")
 }
